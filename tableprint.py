06b7e615367a828fa62958ca66b5af1d1c4a81e2
Keshav <kumarmishra0301@gmail.com>
2025-06-28 12:56:18 +0530

    print multiplication table

---
+++ tableprint.py
@@ -0,0 +1,17 @@
+# Print Mutiplication Table 
+nums = int(input("Enter a number to print its multiplication table: "))
+for i in range(1, 11):
+    print(f"{nums} x {i} = {nums * i}")
+# For example print multiplication table of 2 enter 2 and run the code same as for 3,4,5,etc.
+# Output:
+# Enter a number to print its multiplication table: 2
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
+# 2 x 6 = 12
+# 2 x 7 = 14
+# 2 x 8 = 16
+# 2 x 9 = 18
+# 2 x 10 = 20

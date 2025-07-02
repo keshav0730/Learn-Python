889574d7d0133b5397ce34c7fac416fd977a27ec
Keshav <kumarmishra0301@gmail.com>
2025-07-02 23:31:41 +0530

    Palindrome more programs

--- Palindrome_Checker.py
+++ Palindrome_Checker.py
@@ -5,3 +5,20 @@ if text == text[::-1]:
 else:
     print("Not a palindrome")
 
+
+
+# Ignore case (so "Madam" works too):
+text = input("Enter a word: ").lower()
+if text == text[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+
+
+
+# Check full sentence (ignoring spaces):
+text = input("Enter a sentence: ").replace(" ", "").lower()
+if text == text[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")

025612de5341489f7bf07f34ad5203a641b0dc95
Keshav <kumarmishra0301@gmail.com>
2025-07-09 18:00:18 +0530

    secure login system with 3 attempt

---
+++ secure_login_system_with_3_attempt.py
@@ -0,0 +1,19 @@
+# secure login system with 3 attempts — this is a real-world concept used in many applications.
+import getpass
+
+for i in range(3):
+    username = input("Enter your username: ")
+    password = getpass.getpass("Enter your password: ")
+
+    if username == "admin" and password == "pass123":
+        print("Login successful!")
+        break
+    else:
+        print("Login failed. Please try again.\n")
+
+else:
+    print("Too many attempts. Access denied.")
+# This code allows a user to attempt to log in up to three times.
+
+        
+

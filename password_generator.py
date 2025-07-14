10f7a8778e6f212ea45a8caf0b5730df02b07bfc
Keshav <kumarmishra0301@gmail.com>
2025-07-14 23:47:19 +0530

    password_generator

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+# Generates a random password with letters, numbers, and symbols.
+import random
+import string
+
+length = int(input("Enter password length: "))
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters) for _ in range(length))
+print("Generated password:", password)
